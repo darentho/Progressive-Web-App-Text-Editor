82ad9ee859bcf69e7b7ed4c41715169cabb801a7
Alberto Monroy <firewind3882@gmail.com>
2022-11-15 21:16:45 -0600

    added the code for install.js inside client/src/js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,10 +6,23 @@ window.addEventListener("beforeinstallprompt", (event) => {
   console.log(`event ${event}`);
   event.preventDefault();
   window.deferredPromt = event;
+  butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  const eventPrompt = window.deferredPrompt;
+  if (!eventPrompt) {
+    return;
+  }
+  eventPrompt.prompt();
+
+  window.deferredPrompt = null;
+  butInstall.classList.toggle("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("install hit");
+  window.deferredPrompt = null;
+});
